392bacaeec2c1742d179189df4cd948b97588379
Krishna <krishnag54888@gmail.com>
2024-12-23 14:03:16 +0530

    Mypro1

--- app1/views.py
+++ app1/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    v='This is Home Page'
+    return render(request,'home.html',{'data':v})
 
 def login(request):
-    return render(request,'login.html')
+    l='This is login Page'
+    return render(request,'login.html',{'data1':l})
 
 def signup(request):
     return render(request,'signup.html') 
